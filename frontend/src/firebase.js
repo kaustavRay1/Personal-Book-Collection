098db830c772ef67e03157429bf7217d8dca098e
kaustavRay1 <kaustavray2004@gmail.com>
2026-08-29 20:39:30 +0530

    auth

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -10,8 +10,6 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
-
-console.log("API Key being used:", import.meta.env.VITE_FIREBASE_API_KEY);
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
